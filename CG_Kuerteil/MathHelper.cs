e03d268168377224ca76d66614f61ff4ba2615ad
FlorianSymmank <floriansymmank@gmx.de>
2022-06-12 18:14:46 +0200

    helper to determine x and y of circle for angle + radius

---
+++ CG_Kuerteil/MathHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CG_Kuerteil
+{
+    public static class MathHelper
+    {
+        public static float CircleX(double angle, float radius = 1)
+        {
+            return radius * (float)Math.Cos(OpenTK.Mathematics.MathHelper.DegreesToRadians(angle));
+        }
+
+        public static float CircleY(double angle, float radius = 1)
+        {
+            return radius * (float)Math.Sin(OpenTK.Mathematics.MathHelper.DegreesToRadians(angle));
+        }
+    }
+}
